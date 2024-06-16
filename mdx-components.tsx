54b62bd70e5ebd022b094a01022a52e5c1c420e6
Parth Roy <45533706+psarathi012@users.noreply.github.com>
2024-06-17 00:35:10 +0530

    added more docs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -9,6 +9,7 @@ import { Step, Steps } from "fumadocs-ui/components/steps";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import { TypeTable } from "fumadocs-ui/components/type-table";
 
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
